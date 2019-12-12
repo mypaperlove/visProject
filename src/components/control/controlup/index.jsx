e95f161015df2b09facb38d26d97cf287220401a
viaduct <jizhuoli@scuvis.org>
2019-12-12 15:27:56 +0800

    新增了控制面板,标签选择，地区选择和滚动条

--- src/components/control/controlup/index.jsx
+++ src/components/control/controlup/index.jsx
@@ -1,10 +1,71 @@
 import React, { Component } from 'react'
 import './controlup.css'
+import { Layout, Button, Cascader, Icon } from 'antd';
+import CH_PRO_CITY from '../controlup/CH_PRO_CITY'
+
 export default class Controlup extends Component {
+  state = {
+    province: '省份',
+    city: '城市',
+  }
+//点击不同标签的事件函数
+  clickButtonA(){
+    console.log('点击A按钮事件触发')
+  }
+  clickButtonB(){
+    console.log('点击B按钮事件触发')
+  }
+  clickButtonC(){
+    console.log('点击C按钮事件触发')
+  }
+  clickButtonD(){
+    console.log('点击D按钮事件触发')
+  }
+
+  onChange(value) {
+    this.setState({
+      province: value[0],
+      city: value[1]
+    })
+    console.log(value);
+  }
+
+  componentDidMount(){
+    //显示中国城市数据
+    //console.log(CH_PRO_CITY)
+  }
+
   render () {
+    const { Header, Content } = Layout;
+    const options = CH_PRO_CITY['data'];
     return (
       <div id='controlup'>
-
+        <Layout>
+          <Header>
+            <Icon type="user" />
+            控制面板
+          </Header>
+          
+          <Content>
+          <div>
+            <Button id="ButtonA" onClick={this.clickButtonA}>985</Button>
+            <Button id="ButtonB" onClick={this.clickButtonB}>211</Button>
+            
+          </div>
+          <div>
+            <Button id="ButtonC" onClick={this.clickButtonC}>一流高校</Button>
+            <Button id="ButtonD" onClick={this.clickButtonD}>一流学科高校</Button>
+          </div>
+          <div>
+            <Cascader
+              defaultValue={['北京市','海淀区']}
+              showSearch='true'
+              options={options}
+              onChange={this.onChange.bind(this)}
+            />
+          </div>
+          </Content>
+        </Layout>
       </div>
     )
   }
