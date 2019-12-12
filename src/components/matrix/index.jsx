4db675cfc182ada06e50dd6a02becbffce65c829
llxsgdsg <550304366@qq.com>
2019-12-12 16:24:49 +0800

    更改矩阵颜色

--- src/components/matrix/index.jsx
+++ src/components/matrix/index.jsx
@@ -14,8 +14,7 @@ export default class Matrix extends Component {
   componentDidMount () {
     let hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
       '7a', '8a', '9a', '10a', '11a',
-      '12p', '1p', '2p', '3p', '4p', '5p',
-      '6p', '7p', '8p', '9p', '10p', '11p'
+      '12p', '1p'
     ];
     let days = ['Saturday', 'Friday', 'Thursday',
       'Wednesday', 'Tuesday', 'Monday', 'Sunday'
@@ -132,8 +131,8 @@ export default class Matrix extends Component {
       },
       animation: true,
       grid: {
-        // height: '50%',
-        // y: '10%'
+        //height: '50%',
+        //y: '10%'
       },
       xAxis: {
         position: 'top',
@@ -162,7 +161,7 @@ export default class Matrix extends Component {
         calculable: true,
         orient: 'vertical',
         right: 'right',
-
+        color: ["#3686e7", "#bcfdd6"],
         bottom: '15%'
       },
       series: [{
@@ -171,7 +170,7 @@ export default class Matrix extends Component {
         data: data,
         label: {
           normal: {
-            // show: true
+            show: true
           }
         },
         itemStyle: {
@@ -182,6 +181,165 @@ export default class Matrix extends Component {
         }
       }]
     };
+
+    let option = 
+    {
+      "grid": {
+          "top": 80
+      },
+      "tooltip": {
+          "show": true
+      },
+      "xAxis": {
+          "position": "top",
+          "name": "月份",
+          "type": "category",
+          "data": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "合计"],
+          "splitArea": {
+              "show": true
+          },
+          "axisLine": {
+              "show": false
+          },
+          "axisTick": {
+              "show": false
+          }
+      },
+      "yAxis": {
+          "nameLocation": "end",
+          "type": "category",
+          "data": ["合计", "昆明", "玉溪", "城市名称", "或者", "站点名称", "*国控站点"],
+          "axisLine": {
+              "show": false
+          },
+          "axisTick": {
+              "show": false
+          },
+          "axisLabel": {
+              "rich": {
+                  "red": {
+                      "color": "#f00"
+                  }
+              }
+          },
+          "splitArea": {
+              "show": true
+          }
+      },
+      "visualMap": {
+          "min": 0,
+          "max": 31,
+          "calculable": true,
+          "orient": "horizontal",
+          "right": "30%",
+          "top": "10",
+          "color": ["#3686e7", "#bcfdd6"]
+      },
+      "series": [{
+          "name": "优良天数",
+          "type": "heatmap",
+          "data": [
+              [0, 0, 180],
+              [0, 1, 13],
+              [0, 2, 5],
+              [0, 3, 15],
+              [0, 4, 16],
+              [0, 5, 17],
+              [0, 6, 5],
+              [1, 0, 180],
+              [1, 1, 15],
+              [1, 2, 1],
+              [1, 3, 16],
+              [1, 4, 17],
+              [1, 5, 18],
+              [1, 6, 1],
+              [2, 0, 50],
+              [2, 1, 16],
+              [2, 2, 2],
+              [2, 3, 17],
+              [2, 4, 18],
+              [2, 5, 5],
+              [2, 6, 2],
+              [3, 0, 80],
+              [3, 1, 17],
+              [3, 2, 13],
+              [3, 3, 18],
+              [3, 4, 5],
+              [3, 5, 8],
+              [3, 6, 13],
+              [4, 0, 30],
+              [4, 1, 18],
+              [4, 2, 15],
+              [4, 3, 5],
+              [4, 4, 8],
+              [4, 5, 30],
+              [4, 6, 15],
+              [5, 0, 180],
+              [5, 1, 5],
+              [5, 2, 16],
+              [5, 3, 8],
+              [5, 4, 30],
+              [5, 5, 18],
+              [5, 6, 16],
+              [6, 0, 50],
+              [6, 1, 8],
+              [6, 2, 17],
+              [6, 3, 30],
+              [6, 4, 18],
+              [6, 5, 5],
+              [6, 6, 17],
+              [7, 0, 21],
+              [7, 1, 30],
+              [7, 2, 18],
+              [7, 3, 18],
+              [7, 4, 5],
+              [7, 5, 1],
+              [7, 6, 18],
+              [8, 0, 21],
+              [8, 1, 18],
+              [8, 2, 5],
+              [8, 3, 5],
+              [8, 4, 1],
+              [8, 5, 2],
+              [8, 6, 5],
+              [9, 0, null],
+              [9, 1, 5],
+              [9, 2, 8],
+              [9, 3, 1],
+              [9, 4, 2],
+              [9, 5, 13],
+              [9, 6, 8],
+              [10, 0, null],
+              [10, 1, 1],
+              [10, 2, 30],
+              [10, 3, 2],
+              [10, 4, 13],
+              [10, 5, 15],
+              [10, 6, 30],
+              [11, 0, null],
+              [11, 1, null],
+              [11, 2, null],
+              [11, 3, 13],
+              [11, 4, 15],
+              [11, 5, 16],
+              [11, 6, null]
+          ],
+          "label": {
+              "normal": {
+                  "show": true
+              }
+          },
+          "itemStyle": {
+              "normal": {
+                  "borderColor": "rgba(255, 255, 255, 0.8)"
+              },
+              "emphasis": {
+                  "shadowBlur": 10,
+                  "shadowColor": "rgba(255, 255, 255, 0.5)"
+              }
+          }
+      }]
+  };
     var midHeatmap = echarts.init(document.getElementById('matrix'));
     midHeatmap.setOption(midHeatmapOption);
   }
