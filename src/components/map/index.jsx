879ff4d75d5380d8706f5114a4e433d8feca4129
mypaperlove <08153470@cumt.edu.cn>
2019-12-13 22:02:40 +0800

    增加图层按钮

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -9,21 +9,74 @@ import table1 from './json/table.json'
 
 import './map.css'
 import geoJson from './china.json'
+import { Switch } from 'antd';
 
 export default class Map extends Component {
+  state = {
+    map: {},
+    BMap: {},
+    overLaylist: [],
+  }
   render () {
     return (
-      <div className='col-md-9' id='map'></div>
+      <>
+        <div className='col-md-9' id='map'>
+        </div>
+        <div id='switchColor'>
+          <Switch defaultChecked onChange={this.onChange.bind(this)} />
+        </div>
+      </>
     )
   }
+
+  onChange (checked) {
+    let map = this.state.map;
+    let overLaylist = [];
+    let BMap = this.state.BMap;
+    if (checked == true) {
+      if (this.state.overLaylist.length == 0) {
+        // console.log(overLaylist);
+        console.log('overLaylist.length 等于0');
+        let provList = [["黑龙江", "#F09ABD"], ["吉林", "#01933F"], ["辽宁", "#FAC300"], ["内蒙古", "#FCF502"], ["河北", "#F09ABD"], ["北京", "#FCF502"], ["天津", "#01933F"], ["山东", "#FCF502"], ["江苏", "#D8EDDA"], ["上海", "#B9B4C8"], ["浙江", "#FCF502"], ["福建", "#FAC300"], ["台湾", "#F09ABD"], ["广东", "#FCF502"], ["香港", "#F09ABD"], ["澳门", "#F09ABD"], ["海南", "#F09ABD"], ["广西", "#FAC300"], ["云南", "#FCF502"], ["西藏", "#B9B4C8"], ["新疆", "#FAC300"], ["甘肃", "#01933F"], ["青海", "#F09ABD"], ["四川", "#FAC300"], ["贵州", "#01933F"], ["重庆", "#B9B4C8"], ["湖南", "#F09ABD"], ["江西", "#01933F"], ["湖北", "#FCF502"], ["安徽", "#FAC300"], ["河南", "#B9B4C8"], ["陕西", "#F09ABD"], ["山西", "#01933F"], ["宁夏", "#FAC300"]];
+        setTimeout(function () {
+          provList.forEach(function (item) {
+            getBoundary(item);
+          });
+        }, 20);
+        this.setState({ overLaylist: overLaylist });
+      } else {
+        this.state.overLaylist.forEach((ply) => {
+          map.addOverlay(ply);
+        })
+      }
+    } else {
+      console.log(overLaylist);
+      this.state.overLaylist.forEach((ply) => {
+        console.log('object');
+        map.removeOverlay(ply);
+      })
+    }
+    function getBoundary (provItem) {
+      // console.log(provItem);   
+      var bdary = new BMap.Boundary();
+      bdary.get(provItem[0], function (rs) {       //获取行政区域
+        var count = rs.boundaries.length; //行政区域的点有多少个
+        var pointArray = [];
+        for (var i = 0; i < count; i++) {
+          let ply = new BMap.Polygon(rs.boundaries[i], { strokeWeight: 1, strokeColor: "#aaaaaa", fillColor: provItem[1] }); //建立多边形覆盖物
+          map.addOverlay(ply);  //添加覆盖物
+          overLaylist.push(ply);
+          ply.id = provItem[0];
+          pointArray = pointArray.concat(ply.getPath());
+        }
+      });
+    }
+  }
+
   componentDidMount () {
-
-
-
     const { BMap } = window
 
     baiduMap(BMap);
-
     //画地图的option
     let midMapOption = {
       backgroundColor: 'transparent',
@@ -73,21 +126,21 @@ export default class Map extends Component {
     var midMap = echarts.init(document.getElementById('map'));
     midMap.setOption(midMapOption);
 
-
     //获取map实例   并设置地图样式
     let map = midMap.getModel().getComponent('bmap').getBMap();
+    this.setState(
+      {
+        map: map,
+        BMap: BMap
+      }
+    );
     map.enableMapClick = false;
     map.setMapStyleV2({
       styleId: '8497ad4f7da6e5683f4f5c8e6a9c107d'
     });
 
-    //绘制地图颜色
-    let provList = [["黑龙江", "#F09ABD"], ["吉林", "#01933F"], ["辽宁", "#FAC300"], ["内蒙古", "#FCF502"], ["河北", "#F09ABD"], ["北京", "#FCF502"], ["天津", "#01933F"], ["山东", "#FCF502"], ["江苏", "#D8EDDA"], ["上海", "#B9B4C8"], ["浙江", "#FCF502"], ["福建", "#FAC300"], ["台湾", "#F09ABD"], ["广东", "#FCF502"], ["香港", "#F09ABD"], ["澳门", "#F09ABD"], ["海南", "#F09ABD"], ["广西", "#FAC300"], ["云南", "#FCF502"], ["西藏", "#B9B4C8"], ["新疆", "#FAC300"], ["甘肃", "#01933F"], ["青海", "#F09ABD"], ["四川", "#FAC300"], ["贵州", "#01933F"], ["重庆", "#B9B4C8"], ["湖南", "#F09ABD"], ["江西", "#01933F"], ["湖北", "#FCF502"], ["安徽", "#FAC300"], ["河南", "#B9B4C8"], ["陕西", "#F09ABD"], ["山西", "#01933F"], ["宁夏", "#FAC300"]];
-    setTimeout(function () {
-      provList.forEach(function (item) {
-        getBoundary(item);
-      });
-    }, 20);
+
+
 
 
     let mycomOverlayList = [];     //存放当前省数据
@@ -107,54 +160,35 @@ export default class Map extends Component {
     map.addEventListener("dragend", function showInfo () {
     });
 
-    //地图缩放事件
-    // map.addEventListener('zoomend', function (e) {
-    //   var zoom = e.target.getZoom();
-    //   if (zoom < 17) { // 小于17级，统计图
-    //     myCompOverlay.hide();
-    //   } else {
-    //     myCompOverlay.show();
-    //   }
-    // });
+    // 地图缩放事件
+    map.addEventListener('zoomend', function (e) {
+      var zoom = e.target.getZoom();
+
+      // if (zoom < 17) { // 小于17级，统计图
+      //   myCompOverlay.hide();
+      // } else {
+      //   myCompOverlay.show();
+      // }
+    });
 
 
     //点击事件
     map.addEventListener('click', function (e) {
+      // console.log(e);
       var geocoder = new BMap.Geocoder();
       var point = new BMap.Point(e.point.lng, e.point.lat);
       geocoder.getLocation(point, function (geocoderResult, LocationOptions) {
-        clearOverLay(geocoderResult.address);
+        // clearOverLay(geocoderResult.address);
         drawPieSum(geocoderResult.address.substr(0, 2));
-        map.centerAndZoom(new BMap.Point(mycomOverlayList[0]['data']['lng'], mycomOverlayList[0]['data']['lat']), 10);
+        map.centerAndZoom(new BMap.Point(mycomOverlayList[0]['data']['lng'], mycomOverlayList[0]['data']['lat']), 11);
       });
     })
 
     //去色
-    function clearOverLay (address) {
-      address = address.substr(0, 2);
-      let maplay = map.getOverlays();
-      maplay.forEach(function (params) {
-        if (address === params.id)
-          map.removeOverlay(params);
-      })
-      // console.log(address);
-    }
+
 
     //获取地图边界
-    function getBoundary (provItem) {
-      // console.log(provItem);   
-      var bdary = new BMap.Boundary();
-      bdary.get(provItem[0], function (rs) {       //获取行政区域
-        var count = rs.boundaries.length; //行政区域的点有多少个
-        var pointArray = [];
-        for (var i = 0; i < count; i++) {
-          let ply = new BMap.Polygon(rs.boundaries[i], { strokeWeight: 1, strokeColor: "#aaaaaa", fillColor: provItem[1] }); //建立多边形覆盖物
-          map.addOverlay(ply);  //添加覆盖物
-          ply.id = provItem[0];
-          pointArray = pointArray.concat(ply.getPath());
-        }
-      });
-    }
+
 
 
 
@@ -165,8 +199,11 @@ export default class Map extends Component {
       this._point = point;
     }
 
+    //绘制单个饼图
     var drawPie = function (obj, data) {
-
+      obj.click(function () {
+        console.log('hhhhhhhhhhhhh');
+      })
       let echarts2 = echarts.init(obj);
       let option = {
 
@@ -178,6 +215,8 @@ export default class Map extends Component {
           {
             name: '访问来源',
             type: 'pie',
+            z: 10,
+            zlevel: 10,
             radius: '75%',
             center: ['50%', '50%'],
             data: [
@@ -214,9 +253,12 @@ export default class Map extends Component {
         ]
       };
       echarts2.setOption(option);
+      echarts2.on('click', function (params) {
+        console.log(params);
+      })
     };
 
-
+    //重定义ComplexCustomOverlay
     function baiduMap (baiduMap) {
       /**
        * 自定义覆盖物类, 取名 ComplexCustomOverlay 使用时直接 new ComplexCustomOverlay 即可
@@ -248,6 +290,7 @@ export default class Map extends Component {
         // marginLeft marginTop 的设置可以让这个div的中心点和给定的经纬度重合
         div.style.marginLeft = '-50px';
         div.style.marginTop = '-50px';
+        div.style.zIndex = "10";
         //必须是绝对定位，不然会偏离原来位置
         div.style.position = 'absolute';
         //将该覆盖物添加到标签覆盖物列表
@@ -273,14 +316,14 @@ export default class Map extends Component {
 
     function drawPieSum (province) {
       table1.forEach(function (params) {
-
         if (params['省份'].substr(0, 2) === province) {
-          console.log('111111');
+          // console.log('111111');
           let myCompOverlay = new ComplexCustomOverlay(params.lng, params.lat, drawPie, params);
           map.addOverlay(myCompOverlay);
           let temp = {
             data: params,
-            overlay: myCompOverlay
+            overlay: myCompOverlay,
+            show: true
           };
           mycomOverlayList.push(temp);
         }
