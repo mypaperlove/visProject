930af3dda9dd9864615b99de3a73f0753665fe2e
viaduct <jizhuoli@scuvis.org>
2019-12-23 19:54:39 +0800

    增加了按钮交互

--- src/components/control/controlup/index.jsx
+++ src/components/control/controlup/index.jsx
@@ -9,19 +9,11 @@ export default class Controlup extends Component {
     province: '省份',
     city: '城市',
   }
-  //点击不同标签的事件函数
-  clickButtonA () {
-    console.log('点击A按钮事件触发')
-  }
-  clickButtonB () {
-    console.log('点击B按钮事件触发')
-  }
-  clickButtonC () {
-    console.log('点击C按钮事件触发')
-  }
-  clickButtonD () {
-    console.log('点击D按钮事件触发')
-  }
+
+  clsA = 'Button';
+  clsB = 'Button';
+  clsC = 'Button';
+  clsD = 'Button';
 
   onChange (value) {
     this.setState({
@@ -37,6 +29,28 @@ export default class Controlup extends Component {
   }
   clickButton = (params, e) => {
     this.props.buttontoMap(params);
+    if(params == '985'){
+      this.clsA = 'ButtonClickedA'
+      this.clsB = 'Button'
+      this.clsC = 'Button'
+      this.clsD = 'Button'
+    } else if(params == '211'){
+      this.clsB = 'ButtonClickedB'
+      this.clsA = 'Button'
+      this.clsC = 'Button'
+      this.clsD = 'Button'
+    } else if(params == '双一流'){
+      this.clsC = 'ButtonClickedC'
+      this.clsA = 'Button'
+      this.clsB = 'Button'
+      this.clsD = 'Button'
+    }else if(params == '一流学科建设高校'){
+      this.clsD = 'ButtonClickedD'
+      this.clsA = 'Button'
+      this.clsB = 'Button'
+      this.clsC = 'Button'
+    }
+    
     // console.log(params);
     // console.log(this);
   }
@@ -53,13 +67,13 @@ export default class Controlup extends Component {
 
           <Content>
             <div id="label">
-              <Button id="Button" shape="round" onClick={this.clickButton.bind(this, '985')}>985</Button>
-              <Button id="Button" shape="round" onClick={this.clickButton.bind(this, '211')}>211</Button>
+              <Button  shape="round" className={this.clsA} onClick={this.clickButton.bind(this, '985')}>985</Button>
+              <Button  shape="round" className={this.clsB} onClick={this.clickButton.bind(this, '211')}>211</Button>
 
             </div>
             <div id="label">
-              <Button id="Button" shape="round" onClick={this.clickButton.bind(this, '双一流')}>一流高校</Button>
-              <Button id="Button" shape="round" onClick={this.clickButton.bind(this, '一流学科建设高校')}>一流学科高校</Button>
+              <Button  shape="round" className={this.clsC} onClick={this.clickButton.bind(this, '双一流')}>一流高校</Button>
+              <Button  shape="round" className={this.clsD} onClick={this.clickButton.bind(this, '一流学科建设高校')}>一流学科高校</Button>
             </div>
             <div>
               <Cascader
